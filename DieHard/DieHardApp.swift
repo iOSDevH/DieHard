49cb95a2f3ed569c2614a0f46d21da1ba4cf01bb
iOSDevHeath <iOSDevHeath@icloud.com>
2022-01-19 20:45:23 -0500

    Initial Commit

---
+++ DieHard/DieHardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DieHardApp.swift
+//  DieHard
+//
+//  Created by Heath Fashina on 2022-01-19.
+//
+
+import SwiftUI
+
+@main
+struct DieHardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
